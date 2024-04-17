20c4ce56446810c20fe699cfcf04f67f6954fefd
Lucas Clupny <clupnyluke@clupny.com>
2024-04-17 16:45:50 -0400

    _13

--- _13/src/main.rs
+++ _13/src/main.rs
@@ -1,15 +1,17 @@
+use num_bigint::BigUint;
+use num_traits::Zero;
 use std::io::Read;
 
 fn main() {
     let mut file = std::fs::File::open("nums.txt").unwrap();
     let mut contents = String::new();
-    file.read_to_string(&mut contents);
+    let res = file.read_to_string(&mut contents);
+    res.unwrap();
 
     let strings: Vec<&str> = contents.split(" ").collect();
-    let mut sum: u128 = 0;
+    let mut sum: BigUint = Zero::zero();
     for string in strings {
-        println!("{}", sum);
-        sum += string.parse::<u128>().unwrap();
+        sum += string.parse::<BigUint>().unwrap();
     }
     print!("{}", sum.to_string().get(0..10).unwrap())
 }
